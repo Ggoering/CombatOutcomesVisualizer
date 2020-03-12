ba849a715166295b2569e5e738fe41e362f9433b
George Goering <ggoeringgg@gmail.com>
2020-03-12 10:27:37 -0600

    attacks done, need to test. should have tested first xD

---
+++ src/main/java/com/GG/T9AgeCombat/service/AttackQuantityService.java
@@ -0,0 +1,56 @@
+package com.GG.T9AgeCombat.service;
+
+import com.GG.T9AgeCombat.models.Unit;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AttackQuantityService {
+    public AttackQuantityService(){}
+
+    Integer determineAttackQuantity(Unit attacker, Unit primary, Unit secondary) {
+
+        Unit defender = attacker.getName().equals(primary.getName()) ? secondary : primary;
+        Integer attackerActualWidth = this.determineActualWidth(attacker);
+        Integer defenderActualWidth = this.determineActualWidth(defender);
+
+        Integer modelsNotInB2B = this.determineModelsNotInBaseContact(attackerActualWidth, defender, defenderActualWidth);
+
+        Integer supportingAttacks = this.determineSupportingAttacks(attacker, modelsNotInB2B);
+
+        Integer frontRankAttacks = this.determineFrontRankAttacks(attacker, modelsNotInB2B);
+
+        return supportingAttacks + frontRankAttacks;
+    }
+
+    Integer determineModelsNotInBaseContact(Integer attackerActualWidth, Unit defender, Integer defenderActualWidth) {
+        if(attackerActualWidth - defenderActualWidth <= 0) {
+                return 0;
+            }
+        return Math.round((attackerActualWidth - defenderActualWidth) / defender.getBaseSize()) / 2;
+    }
+
+    Integer determineSupportingAttacks(Unit attacker, Integer modelsNotInB2B) {
+        if(attacker.getName().equals("mount")) {
+            return 0;
+        }
+
+        Integer supportingRanks = 1;
+        if (attacker.getWidth() >= 10) {
+            supportingRanks++;
+        }
+        Integer potentialSupports = attacker.getCount() - attacker.getWidth() - supportingRanks * modelsNotInB2B;
+        return potentialSupports >= supportingRanks * (attacker.getWidth() - modelsNotInB2B) ?
+                supportingRanks * (attacker.getWidth() - modelsNotInB2B) : potentialSupports;
+    }
+
+    Integer determineFrontRankAttacks(Unit attacker, Integer modelsNotInB2B) {
+        return attacker.getWidth() - modelsNotInB2B > attacker.getCount() ? (attacker.getWidth()-modelsNotInB2B)*attacker.getA() : attacker.getCount() * attacker.getA();
+    }
+
+    Integer determineActualWidth(Unit unit) {
+        Integer unitActualWidth = unit.getCount() >= unit.getWidth() ? unit.getWidth() * unit.getBaseSize() : unit.getCount() * unit.getBaseSize();
+
+        return unitActualWidth;
+    }
+
+}
