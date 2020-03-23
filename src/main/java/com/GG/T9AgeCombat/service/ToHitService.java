1ab0e1e35629fdff40c8bf036dffcab2cde5a474
George Goering <ggoeringgg@gmail.com>
2020-03-23 11:05:56 -0600

    Adds and begins to hit service, no tests yet

---
+++ src/main/java/com/GG/T9AgeCombat/service/ToHitService.java
@@ -0,0 +1,37 @@
+package com.GG.T9AgeCombat.service;
+
+import com.GG.T9AgeCombat.models.Unit;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ToHitService {
+    DiceRollingService diceRollingService;
+    public ToHitService(DiceRollingService diceRollingService){
+        this.diceRollingService = diceRollingService;
+    }
+
+    List<Integer> rollAttacks(Integer quantity) {
+        return diceRollingService.roll(quantity);
+    }
+
+    Integer rollToHit(Unit attacker, Unit defender, Integer quantity) {
+        Integer attackerWS = attacker.getOWS();
+        Integer defenderWS = defender.getDWS();
+        List<Integer> resultList = this.rollAttacks(quantity);
+        Integer toHitThreshold = this.determineToHitThreshold(attackerWS, defenderWS);
+
+        return (int)resultList.stream().filter(a -> a >= toHitThreshold).count();
+    }
+
+    Integer determineToHitThreshold(Integer attackerWS, Integer defenderWS) {
+        if(attackerWS == defenderWS) {
+            return 4;
+        } else if(attackerWS > defenderWS) {
+            return attackerWS - defenderWS > 3 ? 2 : 3;
+        } else {
+            return attackerWS - defenderWS < -3 ? 5: 4;
+        }
+    }
+}
