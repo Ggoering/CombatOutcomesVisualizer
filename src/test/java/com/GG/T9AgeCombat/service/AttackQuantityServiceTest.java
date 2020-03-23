33041219f5fb4c5157efce96e52165d3ef89e1cb
George Goering and Jon Kammo <pair+george+jon@pivotal.io>
2020-03-23 09:08:30 -0600

    finished supporting attacks and testing

--- src/test/java/com/GG/T9AgeCombat/service/AttackQuantityServiceTest.java
+++ src/test/java/com/GG/T9AgeCombat/service/AttackQuantityServiceTest.java
@@ -53,11 +53,33 @@ class AttackQuantityServiceTest {
     @Test
     void determineSupportingAttacks() {
         Unit blorcs = Unit.builder().name("Black Orcs").M(4).OWS(5).DWS(5).S(4).T(4).I(2).W(1).A(1).Ld(8).baseSize(25).Count(25).AS(4).width(10).build();
-        Integer BONotInB2B = 4;
+        Unit blorcs2 = Unit.builder().name("Black Orcs").M(4).OWS(5).DWS(5).S(4).T(4).I(2).W(1).A(1).Ld(8).baseSize(25).Count(11).AS(4).width(10).build();
+        Unit blorcs3 = Unit.builder().name("Black Orcs").M(4).OWS(5).DWS(5).S(4).T(4).I(2).W(1).A(1).Ld(8).baseSize(25).Count(10).AS(4).width(10).build();
+        Unit blorcs4 = Unit.builder().name("Black Orcs").M(4).OWS(5).DWS(5).S(4).T(4).I(2).W(1).A(1).Ld(8).baseSize(25).Count(30).AS(4).width(10).build();
+        Unit blorcs5 = Unit.builder().name("Black Orcs").M(4).OWS(5).DWS(5).S(4).T(4).I(2).W(1).A(1).Ld(8).baseSize(25).Count(30).AS(4).width(10).build();
+        Unit blorcs6 = Unit.builder().name("Black Orcs").M(4).OWS(5).DWS(5).S(4).T(4).I(2).W(1).A(1).Ld(8).baseSize(25).Count(3).AS(4).width(10).build();
+        Unit blorcs7 = Unit.builder().name("Black Orcs").M(4).OWS(5).DWS(5).S(4).T(4).I(2).W(1).A(1).Ld(8).baseSize(25).Count(9).AS(4).width(5).build();
+        Unit blorcs8 = Unit.builder().name("Black Orcs").M(4).OWS(5).DWS(5).S(4).T(4).I(2).W(1).A(1).Ld(8).baseSize(25).Count(55).AS(4).width(5).build();
+        Integer BONotInB2B1 = 4;
+        Integer BONotInB2B2 = 0;
 
-        Integer supportAttacks = subject.determineSupportingAttacks(blorcs, BONotInB2B);
+        Integer supportAttacks = subject.determineSupportingAttacks(blorcs, BONotInB2B1);
+        Integer supportAttacks2 = subject.determineSupportingAttacks(blorcs2, BONotInB2B1);
+        Integer supportAttacks3 = subject.determineSupportingAttacks(blorcs3, BONotInB2B1);
+        Integer supportAttacks4 = subject.determineSupportingAttacks(blorcs4, BONotInB2B1);
+        Integer supportAttacks5 = subject.determineSupportingAttacks(blorcs5, BONotInB2B2);
+        Integer supportAttacks6 = subject.determineSupportingAttacks(blorcs6, BONotInB2B2);
+        Integer supportAttacks7 = subject.determineSupportingAttacks(blorcs7, BONotInB2B2);
+        Integer supportAttacks8 = subject.determineSupportingAttacks(blorcs8, BONotInB2B2);
 
         assertEquals(11, supportAttacks);
+        assertEquals(1, supportAttacks2);
+        assertEquals(0, supportAttacks3);
+        assertEquals(12, supportAttacks4);
+        assertEquals(20, supportAttacks5);
+        assertEquals(0, supportAttacks6);
+        assertEquals(4, supportAttacks7);
+        assertEquals(5, supportAttacks8);
     }
 
     @Test
