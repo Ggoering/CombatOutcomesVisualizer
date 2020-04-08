46ffafcfd6f55c9e5977c2034e0e8677c90019e1
George Goering and Jon Kammo <pair+george+jon@pivotal.io>
2020-04-07 21:54:13 -0600

    Fixes and tests dice rolling service

--- src/main/java/com/GG/T9AgeCombat/service/CombatAggregationService.java
+++ src/main/java/com/GG/T9AgeCombat/service/CombatAggregationService.java
@@ -22,11 +22,11 @@ public class CombatAggregationService {
     List<Result> getCombatResults(Integer primaryId, Integer primaryCount, Integer primaryWidth, Integer secondaryId, Integer secondaryCount, Integer secondaryWidth) {
 
         List<Result> resultList = new ArrayList<Result>();
-        Unit swordmaster = Unit.builder().name("Sword Master").M(5).OWS(6).DWS(6).S(5).T(3).I(6).W(1).A(2).Ld(8).baseSize(20).Count(30).AS(5).width(7).selection(1).standardBearer(1).musician(1).build();
+        Unit swordmaster = Unit.builder().name("Sword Master").M(5).OWS(6).DWS(6).S(5).T(3).I(6).W(1).A(2).Ld(8).baseSize(20).Count(30).AS(5).width(5).selection(1).standardBearer(1).musician(1).build();
 
-        Unit blorcs = Unit.builder().name("Black Orcs").M(4).OWS(5).DWS(5).S(4).T(4).I(2).W(1).A(2).Ld(8).baseSize(25).Count(4).AS(4).width(10).selection(1).standardBearer(1).musician(1).build();
+        Unit blorcs = Unit.builder().name("Black Orcs").M(4).OWS(5).DWS(5).S(4).T(4).I(2).W(1).A(2).Ld(8).baseSize(25).Count(25).AS(4).width(10).selection(1).standardBearer(1).musician(1).build();
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10000; i++) {
             Result result = combatCalculationService.combat(swordmaster, blorcs);
 
             resultList.add(result);
